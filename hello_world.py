ec71ac842c07dc2638d867ded1f90c82ace6e429
{CesarAlvarezMTP} <{cesar.alvarez@mtp.es}>
2023-07-04 13:45:06 +0200

    Agregamos FizzBuzz

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,15 @@
-print("hola mundo")
+# print("hola mundo")
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+# Ejemplo de uso
+fizzbuzz(20)
