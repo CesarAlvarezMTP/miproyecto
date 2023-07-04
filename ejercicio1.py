f005ea787ba6e38d0f8f566ffc2705858b711859
{CesarAlvarezMTP} <{cesar.alvarez@mtp.es}>
2023-07-04 16:05:53 +0200

    agrego nuevo archivo

---
+++ ejercicio1.py
@@ -0,0 +1,16 @@
+"""Escribe, en Python, un programa que muestre en pantalla los números del 0 al 100,
+sustituyendo los múltiplos de 3 por la palabra "Fizz" y, a su vez, los múltiplos de 5 por "Buzz".
+Cuando, al mismo tiempo, son múltiplos de 3 y 5, utiliza 'FizzBuzz'"""
+
+def fizzBuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzBuzz(40)
