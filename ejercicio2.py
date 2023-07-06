3e13c04f280e0649c58f84687cfb937f123a05dd
{CesarAlvarezMTP} <{cesar.alvarez@mtp.es}>
2023-07-06 15:57:27 +0200

    Mejorado

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,5 +1,4 @@
-""""[Ayer 18:48] Alejandro Manzanero
-
+"""
 El ejercicio para mañana miércoles sería el siguiente:
 
 Dada esta url = 'https://mtp.alejmans.dev/maas/proxy/test/suma'
@@ -7,13 +6,13 @@ Dada esta url = 'https://mtp.alejmans.dev/maas/proxy/test/suma'
 pasar los parámetros "a=1" y "b=2" como parámetros de una petición GET e imprimir el resultado
 pasar un diccionario {'a':1,'b'=2} como payload de una petición POST e imprimir el resultado
 si el resustado en ambos casos no es 3 elevar una excepción: AssertionError()
-"""
 
+"""
 import requests
 
 URL = 'https://mtp.alejmans.dev/maas/proxy/test/suma'
 
-A = 1
+A = 3
 B = 2
 
 parametros = {
@@ -23,16 +22,19 @@ parametros = {
 
 response = requests.get(URL, params=parametros)
 
-
-
-
 print(response.text)
 
-if float(response.text) !=3
+if float(response.text) != 5:
     raise AssertionError()
+
 data = {
     "a": A,
     "b": B
 }
 
-response = response.post(URL, params=parametros)
+response = requests.post(URL, json=data)
+
+print(response.json()["result"])
+
+if float(response.json()["result"]) != 5:
+    raise AssertionError()
