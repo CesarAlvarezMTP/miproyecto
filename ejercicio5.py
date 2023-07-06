80cbcac513516dc04963571298df92590fce9ef4
{CesarAlvarezMTP} <{cesar.alvarez@mtp.es}>
2023-07-06 18:01:26 +0200

    ok

--- ejercicio5.py
+++ ejercicio5.py
@@ -6,31 +6,28 @@ Mostrar por pantalla el nombre de las personas con más de $50 en esta tabla: ht
 from selenium import webdriver
 
 URL = "https://the-internet.herokuapp.com/tables"
-# Inicializar el navegador y abrir la URL
+
 driver = webdriver.Chrome()
 driver.get(URL)
 
-# Encontrar la tabla y obtener los datos de cada fila
 tabla = driver.find_element_by_id("table1")
 filas = tabla.find_elements_by_css_selector('tbody tr')
 
 datos_filtrados = []
 
-for fila in filas:
-    # Obtener los datos de cada columna en la fila
-    columnas = fila.find_elements_by_css_selector('td')
-    nombre = columnas[0].text
-    apellido = columnas[1].text
-    edad = columnas[2].text
-    sueldo = columnas[3].text.replace('$', '').replace(',', '')
+try:
+    for fila in filas:
+            columnas = fila.find_elements_by_css_selector('td')
+            apellidos = columnas[0].text
+            nombre = columnas[1].text
+            sueldo = columnas[3].text.replace('$', '').replace(',', '')
 
-    # Filtrar por sueldo mayor a $50
-    if float(sueldo) > 50:
-        datos_filtrados.append((nombre, apellido, edad, sueldo))
+            if float(sueldo) > 50:
+                datos_filtrados.append((nombre, apellidos, sueldo))
 
-# Imprimir los nombres de las personas con sueldo mayor a $50
-for datos in datos_filtrados:
-    print(datos[0])
 
-# Cerrar el navegador
-driver.quit()
+    for datos in datos_filtrados:
+        print(datos[0],datos[1],datos[2])
+
+finally:
+    driver.quit()
